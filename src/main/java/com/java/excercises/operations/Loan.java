e3eecf5fc2a738fff3bc91a677533be11a235c62
Mariana-Roldan <marolguin1411@gmail.com>
2020-12-10 21:59:26 -0600

    Salario

---
+++ src/main/java/com/java/excercises/operations/Loan.java
@@ -0,0 +1,47 @@
+package com.java.excercises.operations;
+
+public class Loan {
+	
+	public double getCalculateLoan(double salary) {
+		double loan = 0.0;
+		
+		if(salary>=80000) {
+			loan = 5000000;
+		}
+		else if (salary>=50000 && salary >80000) {
+			loan =3000000;
+			
+		}
+		else if(salary>=20000 && salary>50000) {
+			loan = 15000000;
+			
+		}
+			
+		
+		return loan;
+	}
+	
+	public double getCalculateInterest(double loan) {
+		double bankInterest =0.0;
+		
+		bankInterest = ( (loan)+ (loan*.05));
+		
+		return bankInterest;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		Loan objectLoan = new Loan();
+		Loan objectInterest = new Loan();
+		double result;
+		double resultInterest;
+		result = objectLoan.getCalculateLoan(90000);
+		resultInterest = objectInterest.getCalculateInterest (result);
+		System.out.println("El prestamo es de : $"+ result + "Con un interés de: $"+ resultInterest);
+		
+		
+	}
+	
+
+}
